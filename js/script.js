e199301abf4ea0742510c9f575f38ab80d798c68
harrelchris <24297899+harrelchris@users.noreply.github.com>
2024-09-05 08:45:31 -0500

    Remove nav actions

--- js/script.js
+++ js/script.js
@@ -1,10 +1,1 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const navLinks = document.querySelectorAll('.nav-link');
-
-  navLinks.forEach(link => {
-    link.addEventListener('click', function() {
-      navLinks.forEach(nav => nav.classList.remove('active', 'text-dark'));
-      this.classList.add('active', 'text-dark');
-    });
-  });
-});
+document.getElementById("year").innerText = new Date().getFullYear().toString()
